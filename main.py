bf577386371b230bbb337716e1b4755151f6a52e
AhsokaTa <sanchz.cris@gmail.com>
2023-06-23 22:40:10 +0200

    Changes main to translate the weather and mention activities

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ from config import APIKEY_WEATHER,  APIKEY_OpenAI
 def get_weather_information(query):
 
     url = "http://api.weatherstack.com/current"
-
-    # access_key = APIKEY    
-    #query="Barcelona"
-    # request parameters
     
     parameters = {
         "access_key": APIKEY_WEATHER,    
         "query": query
     }
 
-    # GET request to the API
     response = requests.get(url, params = parameters)
 
     if response.status_code == 200:    
@@ -23,16 +18,14 @@ def get_weather_information(query):
         data = response.json()
         temperature = data["current"]["temperature"]
         description = data["current"]["weather_descriptions"][0]
-        #return temperature, description
 
-        print(f"Temperatura en {query}: {temperature} º C")
-        print(f"Descripción del clima: {description}")
+        return (temperature, description)
+
     else:
         print ("Error")
-        #return None, None
 
 query = input ("¿En qué ciudad quieres conocer el tiempo? ")   
-get_weather_information(query)
+temp , desc = get_weather_information(query)
 
 # https://platform.openai.com/docs/guides/gpt/chat-completions-api
 
@@ -40,7 +33,12 @@ get_weather_information(query)
 openai.api_key = APIKEY_OpenAI
 
 #model https://platform.openai.com/docs/models/overview
-openai.ChatCompletion.create(model = "gpt-3.5-turbo",
-                             messages = [{"role":"user","content": "Que es Python?"}])
+print("La temperatura es ", temp)
+translation = f"Traduce la siguiente frase a castellano: {desc}"
+accion_to_do = f"¿Qué actividad puedo hacer en {query}?"
 
+resp = openai.ChatCompletion.create(model = "gpt-3.5-turbo",
+                             messages = [{"role":"user","content": translation},
+                                         {"role":"user","content": accion_to_do}])
 
+print(resp.choices[0].message.content)
